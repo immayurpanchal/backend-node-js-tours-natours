43f0e3889a0e8f6d5780acf823b311757d3a4265
Mayur Panchal <mayur.panchal@vuclip.com>
2020-02-21 19:48:52 +0530

    mongoose connection established

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,9 +1,27 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+// console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(con => {
+    console.log(con.connections);
+    console.log('Connection Successful');
+  });
 
 const port = process.env.PORT || 3000;
 
