a1d1dcf7f848a3bd31cfaf3492723c2b79cf8399
Mayur Panchal <mayur.panchal@vuclip.com>
2020-02-23 13:05:30 +0530

    removed checkBody middleware as MongoDB handles it

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,19 +1,5 @@
 const tour = require('./../models/tourModel');
 
-// create a checkBody middleware
-// Check if the body contains the name and price property
-// If not, send back 400 (bad request)
-// Add it to the post handler stack
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price'
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
