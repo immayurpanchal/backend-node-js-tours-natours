544c4695055cc8d2515511efe2a48c0c1b0dff7f
Mayur Panchal <mayurpanchal002@gmail.com>
2020-03-09 15:58:23 +0530

    handling of undefined routes

--- app.js
+++ app.js
@@ -37,4 +37,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// If code reaches here means None of the routes matched
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
 module.exports = app;
