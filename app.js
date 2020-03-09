3ef294f33a4ab7f827a49c8bd61fbc193aa3a917
Mayur Panchal <mayurpanchal002@gmail.com>
2020-03-09 16:14:27 +0530

    added error handling middlware

--- app.js
+++ app.js
@@ -38,10 +38,32 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // If code reaches here means None of the routes matched
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'failed',
-    message: `Can't find ${req.originalUrl} on this server`
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'failed';
+  err.statusCode = 404;
+
+  /* 
+  => If next() contains any arg, it will automatically assumes that whatever is passed 
+  is gonna be an error. That rule applies to any middleware function.
+  => If next(args) will be called then it will bypass all the middlware and call the error handling 
+  middleware
+  */
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
