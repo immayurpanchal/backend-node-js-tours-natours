0f6857c9b444d50f9cf7c8ea5705660cda0f0be4
Mayur Panchal <mayurpanchal002@gmail.com>
2020-03-09 16:26:01 +0530

    AppError class and refactoring

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -39,32 +41,9 @@ app.use('/api/v1/users', userRouter);
 
 // If code reaches here means None of the routes matched
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'failed',
-  //   message: `Can't find ${req.originalUrl} on this server`
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'failed';
-  err.statusCode = 404;
-
-  /* 
-  => If next() contains any arg, it will automatically assumes that whatever is passed 
-  is gonna be an error. That rule applies to any middleware function.
-  => If next(args) will be called then it will bypass all the middlware and call the error handling 
-  middleware
-  */
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
