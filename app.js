f720eba022d33d5f6c0a2c183feff237d312c75f
Mayur Panchal <mayurpanchal002@gmail.com>
2020-04-25 11:50:51 +0530

    reviewRoutes added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -82,6 +83,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // If code reaches here means None of the routes matched
 app.all('*', (req, res, next) => {
