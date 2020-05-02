df52dd28ccecc75a4abecc998150e152edd0f4cc
Mayur Panchal <mayurpanchal002@gmail.com>
2020-05-02 18:12:54 +0530

    get distances of all tours from a point

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -119,6 +119,55 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  // same as divinding by 1000 as distance is in meters and we want in kms
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format : lat,lng',
+        400
+      )
+    );
+  }
+
+  /* 
+  There is only one aggregate function to find distances from 
+  particular location. It must be the first aggregate function
+  */
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        // Point from where you want to calculate all distances
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1]
+        },
+        // This field will be created and all the distances will be stored
+        distanceField: 'distance',
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project: {
+        distance: 1, // 1 means true
+        name: 1
+      }
+    }
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances
+    }
+  });
+});
+
 exports.getAllTours = factory.getAll(Tour);
 exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.createTour = factory.createOne(Tour);
--- models/tourModel.js
+++ models/tourModel.js
@@ -187,11 +187,12 @@ tourSchema.post(/^find/, function(docs, next) {
 });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
+/* tourSchema.pre('aggregate', function(next) {
   // console.log(this); // Points to current aggregation object
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
-});
+}); */
 
 const Tour = mongoose.model('Tour', tourSchema);
 
