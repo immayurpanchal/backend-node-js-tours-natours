ca96d45df72f0b6e7293e12ebf98b9cd0cad1cbc
Mayur Panchal <mayurpanchal002@gmail.com>
2020-05-09 16:45:13 +0530

    limit set to 1K

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from this IP, Try again after an hour'
 });
