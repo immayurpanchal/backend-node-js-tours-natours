f1ed1565b6a9e713096a336ca5585fe7940cff3a
Mayur Panchal <mayurpanchal002@gmail.com>
2020-05-09 19:04:56 +0530

    changed DB server to online

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const app = require('./app');
 
 // console.log(process.env);
 
-/* const DB = process.env.DATABASE.replace(
+const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-); */
+);
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
